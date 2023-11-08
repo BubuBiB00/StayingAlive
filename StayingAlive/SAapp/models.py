86af6fc3f3d628bc46868c3de06bdd6d561ce204
IngNoN <sebastian.karner@bulme.at>
2023-11-08 10:55:56 +0100

    create Exercise table

--- StayingAlive/SAapp/models.py
+++ StayingAlive/SAapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Exercise(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=400)
+    path = models.TextField()
+    create_date = models.DateTimeField("date created")
