28ba9cc897dc9bf6716954ab93d270742c409bed
IngNoN <sebastian.karner@bulme.at>
2023-11-22 15:36:52 +0100

    Add upload UI and store paths in db

--- StayingAlive/SAapp/helpers/SFTPConnector.py
+++ StayingAlive/SAapp/helpers/SFTPConnector.py
@@ -4,13 +4,13 @@ from base64 import decodebytes
 keydata = b"""AAAAC3NzaC1lZDI1NTE5AAAAIBXPxzSczLFKF/k0MrNfVAGJXrRcm74WydJti3cWo0Oj"""
 key = paramiko.Ed25519Key(data=decodebytes(keydata))
 
-class FTPConnector:
+class SFTPConnector:
     def __init__(self):
         self.client = paramiko.SSHClient()
         self.client.get_host_keys().add('[sandbox.bulme.at]:22', 'ssh-rsa', key)
         self.client.set_missing_host_key_policy(paramiko.RejectPolicy)
 
-        file = open("StayingAlive/SAapp/helpers/config.txt")
+        file = open("SAapp/helpers/config.txt")
         line = file.readline()
         self.data = line.split(",")
         file.close()
@@ -30,13 +30,17 @@ class FTPConnector:
             print(e)
     
     def upload_video(self, file_to_upload):
+        print(f"file_to_upload {file_to_upload}")
         file_only = file_to_upload.split("/")
+        print(f"file_only {file_only}")
         file_location_on_server = self._serverlocation + file_only[-1]
+        print(f"file_location_on_server {file_location_on_server}")
         
         sftp = self.connect_to_server()
         try:
             sftp.put(file_to_upload, file_location_on_server)
             sftp.close()
+            return(file_location_on_server)
 
         except Exception as e:
             sftp.close()
--- StayingAlive/SAapp/urls.py
+++ StayingAlive/SAapp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView, name="index")]
+    path("", views.IndexView, name="index"),
+    path("upload/", views.upload_exercise, name="upload")]
