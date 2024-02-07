0a1be8080664f1836149879014ab68a35de12c9e
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2024-02-07 16:10:53 +0100

    renamed views

--- StayingAlive/SAapp/views.py
+++ StayingAlive/SAapp/views.py
@@ -1,5 +1,3 @@
-import os
-
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
@@ -7,16 +5,15 @@ from .helpers.SFTPConnector import SFTPConnector
 from django.template import loader
 from .models import Exercise
 from django.utils import timezone
-from os import listdir
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
-from .forms import UserCreationForm, LoginForm
+from django.contrib import messages
 
 from random import randint
 
 # Create your views here.
-def IndexView(request):
+def index_view(request):
     exercise_list = Exercise.objects.all()
     template = loader.get_template('SAapp/index.html')
     context = { "exercise_list" : exercise_list}
@@ -24,7 +21,7 @@ def IndexView(request):
     #output = ", ".join([q.title for q in exercise_list])
     #return HttpResponse(output)
 
-def UploadExerciseView(request):
+def upload_exercise_view(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         post_data = request.POST
@@ -41,7 +38,7 @@ def UploadExerciseView(request):
         })
     return render(request, 'SAapp/uploadExercise.html')
 
-def ExerciseSequenceView(request):
+def exercise_sequence_view(request):
     sequence_length = 5
     training = []
     template = loader.get_template('SAapp/exercise_sequence.html')
@@ -55,7 +52,7 @@ def ExerciseSequenceView(request):
     context = { "exercise_sequence" : training}
     return HttpResponse(template.render(context, request))
 
-def ExerciseListView(request):
+def exercise_list_view(request):
     template = loader.get_template('SAapp/exercise_list.html')
     exercise_list = []
 
@@ -67,32 +64,13 @@ def ExerciseListView(request):
     context = { "exercise_list" : exercise_list}
     return HttpResponse(template.render(context, request))
 
-def LoginView(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)    
-                return redirect('SAapp/loggedin')
-            else:
-                return redirect('SAapp/signin')
-    else:
-        form = LoginForm()
-    return render(request, 'SAapp/login.html', {'form': form})
+def login_view(request):
 
-def SignupView(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserCreationForm()
+    return render(request, 'SAapp/auth/login.html')
 
-    return render(request, 'SAapp/signup.html', {'form': form})
+def signup_view(request):
+
+    return render(request, 'SAapp/auth/signup.html')
 
 def user_logout(request):
     logout(request)
@@ -102,9 +80,3 @@ def watch_exercise_view(request):
     video_name = "sample-5s.mp4"
     return render(request, template_name='SAapp/watchExercise.html', context={"video_to_watch":video_name})
 
-def logged_in_view(request):
-    template = loader.get_template('SAapp/exercise_list.html')
-    user = request.user
-
-    context = {"current_user" : user}
-    return HttpResponse(template.render(context, request))
