8407439c5b5c13e3868aee246c292f71047da633
jakobSPL2021 <jakob.laurin@bulme.at>
2024-02-09 12:51:45 +0100

    added id for edit/delete

--- StayingAlive/SAapp/models.py
+++ StayingAlive/SAapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Exercise(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=400)
     path = models.TextField()
