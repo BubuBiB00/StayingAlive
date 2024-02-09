68b0e54adebdf04cab760e965481ed7956a320cc
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2024-02-09 12:01:02 +0100

    updated urls.py

--- StayingAlive/SAapp/urls.py
+++ StayingAlive/SAapp/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
+    path("watch/<str:video_name>", views.watch_exercise_view, name="watch"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
     path("upload/", views.upload_exercise_view, name="upload"),
     path("exercise_sequence/", views.exercise_sequence_view, name="exercise_sequence"),
     path("exercise_list/", views.exercise_list_view, name="exercise_list"),
-    path("watch/", views.watch_exercise_view, name="watch"),
-    path("login/", views.login_view, name="login"),
-    path("signup/", views.signup_view, name="signup"),
-]
+    path("watch/", views.watch_exercise_view, name="watch")]
